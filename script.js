6ecf61603365e1dc6008f30c99de30d456c278f7
Aparnauk <aparnauk22@gmail.com>
2023-11-27 11:38:04 +0530

    'home'

--- script.js
+++
@@ -1,8 +0,0 @@
-// const navLinkEls = document.querySelectorAll('.nav_link');
-// const windowPathname = window.location.pathname;
-
-// navLinkEls.forEach(navLinkEL => {
-//     if (navLinkEL.href.includes (windowPathname)){
-//         navLinkEL.classList.add('active');
-//     }
-// });
